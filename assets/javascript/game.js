14733e88d22e57c308d2f012f39314385a00d487
Michael Megarbane <MJ@Monas-MacBook.fios-router.home>
2017-05-26 11:05:53 -0400

    some progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,11 @@ var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 
 var correctGuesses = []
 
+var blankarray = []
 
+
+
+// picks a random word from the artist array and places it in the html
 function randomword(){
 var currentArtist = artists[Math.floor(Math.random()*artists.length)];
 
@@ -16,7 +20,6 @@ var splitArtist = currentArtist.split('');
 
 
  var letterPlaces =currentArtist.length 
-alert(letterPlaces)
 
 
 
@@ -29,26 +32,61 @@ for(i=0; i<letterPlaces-1;i++)
 var newspan = document.createElement("span")
 targetdiv.append(newspan)
 newspan.innerHTML = "__ "
-}
-// }
-
 
 }
 
 
-randomword()
-
-
-
-document.getElementById('EnterGuess').onclick = function() {
-document.getElementById('EnterGuess').onkeyup = function(event)
-{
+document.getElementById('EnterGuess').onclick = function startinput(splitArtist) {
+document.getElementById('EnterGuess').onkeyup = function(event) {
  var userinput = event.key;
 
  alert(userinput)
+
+
+for(i=0; i<splitArtist.length; i++)
+
+	{
+	if(userinput===splitArtist[i])
+	{
+		var x = i
+		blankarray.splice(x,1,(userinput))
+		alert("found")
+	}
+
+
 }
 }
 
+
+}
+
+}
+
+randomword()
+for(i=0;i<blankarray.length;i++)
+{
+	console.log(blankarray[i])
+}
+
+
+
+// function tha captures user key press once inside input box
+
+
+
+
+
+function newletter(){
+var targetdiv2 = document.getElementbyId("wordtobeGuessed");
+
+for(j=0;j<blankarray.length;i++)
+
+{
+	var newspan = document.createElement("span")
+	targetdiv2.append(newspan)
+newspan.innerHTML = blankarray[j]
+}
+}
 
 
 
